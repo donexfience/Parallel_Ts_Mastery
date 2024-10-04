77e8f5717347ebe9e359a48b47e0b2f0dd969f7e
donexfience <donex6fience@gmail.com>
2024-10-04 15:29:25 +0530

    day -1 .1

--- util types/excludeType.ts
+++ util types/excludeType.ts
@@ -1,10 +1,17 @@
-type T0 = Exclude<"a" | "b" | "c", "a">;
+// type T0 = Exclude<"a" | "b" | "c", "a">;
 
-type T0 = "b" | "c";
-type T1 = Exclude<"a" | "b" | "c", "a" | "b">;
+// type T0 = "b" | "c";
+// type T1 = Exclude<"a" | "b" | "c", "a" | "b">;
 
-type T1 = "c";
-type T2 = Exclude<string | number | (() => void), Function>;
+// type T1 = "c";
+// type T2 = Exclude<string | number | (() => void), Function>;
 
-type T56 = Exclude<string | number, boolean | true>;
-const num: T56 = false;
+class Newclass {
+  constructor(a: number, b: string) {}
+}
+type T3 = ConstructorParameters<typeof Newclass>;
+function Printing(args: T3): Newclass {
+  return new Newclass(...args);
+}
+
+const val = Printing([3,"donex"]);
